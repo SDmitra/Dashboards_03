de18cc01167b31501a9a74e7cb4e8751885739c9
SDmitra <dmitrystasov@gmail.com>
2023-12-27 11:28:41 +0300

    Initial commit

---
+++ Dashboards_V3.1.py
@@ -0,0 +1,12 @@
+def main():
+    import matplotlib.pyplot as plt
+    import streamlit as st
+    import pandas as pd
+    from datetime import datetime, timedelta
+    import seaborn as sns
+    import numpy as np
+    import plotly.express as px
+    
+# Run the app
+if __name__ == "__main__":
+    main()
